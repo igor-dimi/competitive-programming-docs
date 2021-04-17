130f2c7615fdd790e984c5c1eac1032fa337fb36
igor-dimi <igor-dimi@protonmail.com>
2021-04-17 01:16:20 +0000

    deploy: bdbaf2bca385172a1bcf4fe84ccc37e393e07ce0

---
+++ en.search-data.min.4ca36736e18d7dec97d0d38da95233f0b4b3f6e4b5ad9972c170e8f143cff4cc.js
@@ -0,0 +1,1 @@
+'use strict';(function(){const a={cache:!0};a.doc={id:'id',field:['title','content'],store:['title','href','section']};const b=FlexSearch.create('balance',a);window.bookSearchIndex=b,b.add({id:0,href:'/competitive-programming-docs/',title:"Index",section:"Competitive Programming Docs",content:"Hello World!\n"})})()
